c9c1cfbf238ea4997e1e1883f43bb7f41d2afd54
PrajwalG7 <gaikwadaryan688@gmail.com>
2022-07-13 22:38:36 +0530

    added a filter for rides

--- app/src/main/java/com/example/edvoratask/MainActivity.kt
+++ app/src/main/java/com/example/edvoratask/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.example.edvoratask
 
 import android.graphics.Color
-import android.graphics.Paint
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -26,13 +24,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var result: Call<List<RideList>>
     private lateinit var resultUser: Call<UserList>
     lateinit var  userName:TextView
+    lateinit var  filter:TextView
     private lateinit var RideAPIs:RideAPI
     private lateinit var nearestRides:TextView
     private lateinit var upcomingRides:TextView
     private lateinit var pastRides:TextView
     private lateinit var userLoggedProfile:ImageView
+    private  lateinit var relativeLayout:RelativeLayout
     var userLoggedStationCode:Int=0
     var distance:Int=0
+    lateinit var  spinner:Spinner
+    val filteredList: MutableList<RideList> = ArrayList()
+    var clicked:Int=1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,9 +48,11 @@ class MainActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
 
+        spinner= Spinner(applicationContext)
         userName=findViewById(R.id.IdUserLoggedName)
         nearestRides=findViewById(R.id.nearestRides)
         upcomingRides=findViewById(R.id.UpcomingRides)
+        filter=findViewById(R.id.filter)
         pastRides=findViewById(R.id.pastRides)
         userLoggedProfile=findViewById(R.id.IdUserLoggedImg)
         nearestRides.setTextColor(Color.parseColor("#FFFFFF"))
@@ -61,6 +66,7 @@ class MainActivity : AppCompatActivity() {
     fun nearestRide(view: View) {
         upcomingRides.setTextColor(Color.parseColor("#c2bebe"))
         pastRides.setTextColor(Color.parseColor("#c2bebe"))
+        filter.setTextColor(Color.parseColor("#c2bebe"))
         nearestRides.setTextColor(Color.parseColor("#FFFFFF"))
         forNearestRide()
     }
@@ -68,6 +74,7 @@ class MainActivity : AppCompatActivity() {
     fun upcomingRide(view: View){
         nearestRides.setTextColor(Color.parseColor("#c2bebe"))
         pastRides.setTextColor(Color.parseColor("#c2bebe"))
+        filter.setTextColor(Color.parseColor("#c2bebe"))
         upcomingRides.setTextColor(Color.parseColor("#FFFFFF"))
         forNearestRide()
     }
@@ -76,6 +83,7 @@ class MainActivity : AppCompatActivity() {
         nearestRides.setTextColor(Color.parseColor("#c2bebe"))
         upcomingRides.setTextColor(Color.parseColor("#c2bebe"))
         pastRides.setTextColor(Color.parseColor("#FFFFFF"))
+        filter.setTextColor(Color.parseColor("#c2bebe"))
 
         forNearestRide()
     }
@@ -145,6 +153,85 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
 
     }
-}
 
+    fun filter(view: View) {
+
+            relativeLayout=findViewById(R.id.relLayout)
+
+            if(clicked%2==0){
+                relativeLayout.visibility=View.GONE
+            }
+           else{
+            filter.setTextColor(Color.parseColor("#FFFFFF"))
+            nearestRides.setTextColor(Color.parseColor("#c2bebe"))
+            upcomingRides.setTextColor(Color.parseColor("#c2bebe"))
+            pastRides.setTextColor(Color.parseColor("#c2bebe"))
+
+
+            val relativeLayout = findViewById<RelativeLayout>(R.id.relLayout)
+            relativeLayout.visibility = View.VISIBLE
+
+            val spinnerState = findViewById<Spinner>(R.id.spin_state)
+            val spinnerCity = findViewById<Spinner>(R.id.spin_city)
+
+
+            val listForStates: MutableList<String> = ArrayList()
+            val listForCitiesOfSelectedState: MutableList<String> = ArrayList()
+
+            for (i in re.indices) {
+                listForStates.add(re[i].state)
+            }
+
+            val adapterState = ArrayAdapter(
+                applicationContext,
+                android.R.layout.simple_spinner_item,
+                listForStates
+            )
+            spinnerState.adapter = adapterState
+
+
+            spinnerState.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parentView: AdapterView<*>,
+                    selectedItemView: View,
+                    position: Int,
+                    id: Long
+                ) {
+                    //       Toast.makeText(applicationContext, parentView.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show()
+                    val selectedState = parentView.getItemAtPosition(position)
+
+                    for (i in re.indices) {
+                        if (re[i].state == selectedState) {
+                            listForCitiesOfSelectedState.add(re[i].city)
+                        }
+                    }
+
+                    val adapterCity = ArrayAdapter(
+                        applicationContext,
+                        android.R.layout.simple_spinner_item,
+                        listForCitiesOfSelectedState
+                    )
+                    spinnerCity.adapter = adapterCity
+
+                    for (i in listForCitiesOfSelectedState.indices) {
+                        for (j in re.indices) {
+                            if (re[j].city == listForCitiesOfSelectedState[i]) {
+                                filteredList.add(re[j])
+                            }
+
+                        }
+                    }
+
+                    recyclerView.adapter = MyAdapter(filteredList, distance)
+                }
+
+                override fun onNothingSelected(adapterView: AdapterView<*>?) {}
+            }
+
+
+        }
+        clicked += 1
+    }
+}
+ 
 
